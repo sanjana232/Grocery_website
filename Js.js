54ca467c0a5b4fead9f02cacdeb83437d5c96ce8
Thirumaldas Sanjana <166901236+sanjana232@users.noreply.github.com>
2025-05-01 16:39:36 +0530

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,77 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick= () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    logIn.classList.remove('active');
+    navBar.classList.remove('active');
+}
+let shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick= () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    logIn.classList.remove('active');
+    navBar.classList.remove('active');
+}
+let logIn = document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick= () =>{
+    logIn.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navBar.classList.remove('active');
+}
+let navBar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick= () =>{
+    navBar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    logIn.classList.remove('active');
+    
+}
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    logIn.classList.remove('active');
+    navBar.classList.remove('active');
+}
+var swiper = new Swiper(".product-slidder", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Grocery_website
+# Grocery-website
